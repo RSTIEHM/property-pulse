422cbdaafb90fa01df2337b071fd207c5b09bb60
Richard Stiehm <richardstiehm@MediaArhiveiMac.lan>
2024-03-24 12:19:07 -0400

    Single Details added

--- utils/requests.js
+++ utils/requests.js
@@ -1,5 +1,6 @@
 const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN || null;
 
+// FETCH ALL PROPERTIES
 async function fetchProperties() {
   try {
     // HANDLE WHERE DOMAIN IS NOT AVAILABLE YET
@@ -10,7 +11,6 @@ async function fetchProperties() {
     if (!res.ok) {
       throw new Error("Failed To Fetch");
     }
-    console.log("WE ARE HERE");
     return res.json();
   } catch (e) {
     console.log(e);
@@ -18,4 +18,22 @@ async function fetchProperties() {
   }
 }
 
-export { fetchProperties };
+// FETCH SINGLE PROPERTY
+async function fetchProperty(id) {
+  try {
+    // HANDLE WHERE DOMAIN IS NOT AVAILABLE YET
+    if (!apiDomain) {
+      return null;
+    }
+    const res = await fetch(`${apiDomain}/properties/${id}`);
+    if (!res.ok) {
+      throw new Error("Failed To Fetch");
+    }
+    return res.json();
+  } catch (e) {
+    console.log(e);
+    return null;
+  }
+}
+
+export { fetchProperties, fetchProperty };
